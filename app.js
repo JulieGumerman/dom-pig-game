0dc5a1f5ef44496682974700de6a30b8eae11a2b
bodhibrdr <julie.gumerman@gmail.com>
2019-03-17 18:36:04 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+var scores, roundScore, activePlayer;
+
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+
+
+
+
+
+document.querySelector(".dice").style.display = "none";
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent= "0";
+
+
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+	//random number
+	var dice = Math.floor(Math.random()*6)+1;
+	//display result
+	var diceDOM =document.querySelector(".dice");
+	diceDOM.style.display="block";
+	diceDOM.src = "dice-" + dice + ".png";
+	
+	//update round score IF rolled number !==1
+	if (dice !== 1) {
+		//add score
+		roundScore += dice;
+		document.querySelector("#current-" + activePlayer).textContent = roundScore;
+	} else {
+		//next player
+		activePlayer === 0 ? activePlayer = 1 : activePlayer =0;
+		roundScore = 0;
+
+		document.getElementById("current-0").textContent="0";
+		document.getElementById("current-1").textContent="0";
+
+		document.querySelector(".player-0-panel").classList.toggle('active');
+		document.querySelector(".player-1-panel").classList.toggle('active');
+
+
+	}
+
+});
+
+
+//document.querySelector("#score-" + activePlayer).textContent = dice;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
