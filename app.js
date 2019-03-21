ce1e2ee051c64af29840ee60eabd0234f800dc49
bodhibrdr <julie.gumerman@gmail.com>
2019-03-20 18:16:09 -0600

    finished...for now

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var scores, roundScore, activePlayer;
 
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
+//scores = [0,0];
+//roundScore = 0;
+//activePlayer = 0;
+
+init();
 
 
 
 
 
+//document.querySelector(".dice").style.display = "none";
 
-document.querySelector(".dice").style.display = "none";
-
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent= "0";
+//document.getElementById("score-0").textContent = "0";
+//document.getElementById("score-1").textContent = "0";
+//document.getElementById("current-0").textContent = "0";
+//document.getElementById("current-1").textContent= "0";
 
 
 
@@ -76,7 +77,30 @@ function nextPlayer() {
 
 
 
+document.querySelector(".btn-new").addEventListener("click", init);
 
+
+function init() {
+	scores = [0,0];
+	activePlayer = 0;
+	roundScore = 0;
+
+	document.querySelector(".dice").style.display = "none";
+
+	document.getElementById("score-0").textContent = "0";
+	document.getElementById("score-1").textContent = "0";
+	document.getElementById("current-0").textContent = "0";
+	document.getElementById("current-1").textContent= "0";
+
+	document.getElementById("name-0").textContent = "Player 1";
+	document.getElementById("name-1").textContent = "Player 2";
+
+	document.querySelector(".player-0-panel").classList.remove("winner");
+	document.querySelector(".player-1-panel").classList.remove("winner");
+	document.querySelector(".player-0-panel").classList.remove("active");
+	document.querySelector(".player-0-panel").classList.add("active");
+	document.querySelector(".player-1-panel").classList.remove("active");
+}
 
 
 
